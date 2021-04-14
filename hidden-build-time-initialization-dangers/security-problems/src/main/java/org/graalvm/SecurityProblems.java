17fced93bac493d5b33b12fd18df237379c4f46c
Aleksandar Gradinac <aleksandar.gradinac@oracle.com>
2021-04-15 01:13:30 +0200

    Add security problems example

---
+++ hidden-build-time-initialization-dangers/security-problems/src/main/java/org/graalvm/SecurityProblems.java
@@ -0,0 +1,59 @@
+package org.graalvm;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
+
+public class SecurityProblems
+{
+    // Will leak the user home directory of the user building the image!
+    private static final String USER_HOME = System.getProperty("user.home");
+
+    // Will "bake" the private key found during the image build into the image!
+    private static final PrivateKey runtimeSuppliedPrivateKey;
+
+    static {
+        PrivateKey key = null;
+        try {
+            InputStream privateKeyStream = SecurityProblems.class.getResourceAsStream("/private-key.der");
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(privateKeyStream.readAllBytes());
+            key = KeyFactory.getInstance("RSA").generatePrivate(spec);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        runtimeSuppliedPrivateKey = key;
+    }
+
+    // Will always contain the same random seed at image runtime!
+    private static final SimplePRNG randomNumberGenerator = new SimplePRNG(System.currentTimeMillis());
+
+
+    public static void main(String[] args) {
+        System.out.println("My user home is: " + USER_HOME);
+
+        System.out.println("My private key hash is: " + Arrays.hashCode(runtimeSuppliedPrivateKey.getEncoded()));
+
+        System.out.println("An entirely random sequence: " + randomNumberGenerator.nextRandom() + " " + randomNumberGenerator.nextRandom() + " " + randomNumberGenerator.nextRandom());
+    }
+}
+
+class SimplePRNG {
+
+    private static final long PRIME = 7937;
+    private long seed;
+
+    SimplePRNG(long seed) {
+        this.seed = seed % PRIME;
+    }
+
+    long nextRandom() {
+        seed = (seed * 2) % PRIME;
+        return seed;
+    }
+
+}
