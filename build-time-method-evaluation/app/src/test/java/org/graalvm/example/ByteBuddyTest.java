3af79d640b3214b1f89fd1d56d0d1a274eb265c2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2022-07-26 15:10:23 +0200

    Better examples for bytecode generation

--- build-time-method-evaluation/app/src/test/java/org/graalvm/example/ByteBuddyTest.java
+++ build-time-method-evaluation/app/src/test/java/org/graalvm/example/ByteBuddyTest.java
@@ -7,11 +7,15 @@ import java.lang.reflect.InvocationTargetException;
 
 public class ByteBuddyTest {
 
+    public static final String HELLO_FIRST_CLASS_WORLD = "Hello, First Class World!";
+    public static final String HELLO_SECOND_CLASS_WORLD = "Hello, Second Class World!";
+
     @Test
     public void testGreeting() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        Object greeter = ByteBuddyClassCreator.createGreeter();
-        System.out.println("Created a greeter! The greeter says: " + greeter);
-        Assertions.assertEquals(greeter.toString(), ByteBuddyClassCreator.GREETER_MESSAGE);
+        System.out.println("Created a greeter! The greeter says: " + ByteBuddyClassCreator.createByteBuddyClass(HELLO_FIRST_CLASS_WORLD).getConstructor().newInstance());
+        System.out.println("Created another greeter! The greeter says: " + ByteBuddyClassCreator.createByteBuddyClass(HELLO_SECOND_CLASS_WORLD).getConstructor().newInstance());
+        Assertions.assertEquals(ByteBuddyClassCreator.createByteBuddyClass(HELLO_FIRST_CLASS_WORLD).getConstructor().newInstance().toString(), HELLO_FIRST_CLASS_WORLD);
+        Assertions.assertEquals(ByteBuddyClassCreator.createByteBuddyClass(HELLO_SECOND_CLASS_WORLD).getConstructor().newInstance().toString(), HELLO_SECOND_CLASS_WORLD);
     }
 
 }
